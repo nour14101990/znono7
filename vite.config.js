459412d6f5c407f4ede773b76b7deeeb2ae0fe72
Zakhrouf Noureddine <nour-zakh@hotmail.com>
2025-08-27 14:08:42 +0100

    first commit2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {visualizer} from 'rollup-plugin-visualizer';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+   plugins: [
+    react(),
+    visualizer({ 
+      open: true,
+      filename: 'bundle-analysis.html',
+    }),
+  ],
+  base: process.env.VITE_BASE_PATH ||  './',
+  build: {
+     // Output directory
+    outDir: 'dist',
+    // Generate sourcemaps for debugging (optional)
+    sourcemap: true,
+    // Reduce chunk sizes
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Create vendor chunks based on package names
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom')) {
+              return 'react-vendor';
+            }
+            if (id.includes('antd')) {
+              return 'antd-vendor';
+            }
+            if (id.includes('lodash') || id.includes('axios')) {
+              return 'utils-vendor';
+            }
+            // Group other node_modules into a vendor chunk
+            return 'vendor';
+          }},
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjust warning limit if needed
+    
+  },
+  define: {
+    'process.env': process.env
+  }
 })
